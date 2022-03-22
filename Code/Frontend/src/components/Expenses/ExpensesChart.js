83c67adffcd3071836fb7c799a41d39ad2217a64
agustinllado <lladoagustin@gmail.com>
2022-03-22 16:52:28 -0300

    Added Chart component

---
+++ Code/Frontend/src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,26 @@
+import Chart from "../Chart/Chart";
+
+const ExpensesChart = (props) => {
+  const chartDataPoints = [
+    { label: 'Jam', value: 0 },
+    { label: 'Feb', value: 0 },
+    { label: 'Marc', value: 0 },
+    { label: 'Apr', value: 0 },
+    { label: 'May', value: 0 },
+    { label: 'Jun', value: 0 },
+    { label: 'July', value: 0 },
+    { label: 'Aug', value: 0 },
+    { label: 'Set', value: 0 },    
+    { label: 'Oct', value: 0 },
+    { label: 'Nov', value: 0 },
+    { label: 'Dec', value: 0 }
+  ];
+
+  for (const expense of props.expenses) {
+      const expenseMonth = expense.date.getMonth(); // Starting at 0 => January is 0
+      chartDataPoints[expenseMonth].value += expense.amount;
+  }
+  return <Chart dataPoints={chartDataPoints} />;
+};
+
+export default ExpensesChart;
